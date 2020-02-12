c7dfa5b41a3db95739acfa6f8559fd30a970dd55
Louis Gatinet <louis.gatinet@gmail.com>
2020-02-12 12:22:01 +0100

    with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "There are #{Restaurant.count} restaurants"
+puts "let's destroy all restaurants"
+Restaurant.destroy_all
+puts "let's seed it"
+
+restaurants = [
+  {name: 'Big mama', address:'Lille', category: 'italian'},
+  {name: 'Mamacita', address:'Paris', category: 'french'},
+  {name: 'Pizzeria', address:'Marseille', category: 'chinese'},
+  {name: 'Suhsishop', address:'Toulouse', category: 'japanese'},
+  {name: 'la famille', address:'Montpellier', category: 'french'}
+]
+
+Restaurant.create(restaurants)
+puts "There are #{Restaurant.count} restaurants"
